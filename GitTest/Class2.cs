74d0108ee0b3ea30bd3f6bb3fc727423ee247594
RobertMarti <44323037+RobertMarti@users.noreply.github.com>
2018-12-23 14:30:05 +0100

    Update Branch1.cs

--- GitTest/Class2.cs
+++ GitTest/Class2.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GitTest
 {
-    class Class3
+    class Class2
     {
     }
 }
