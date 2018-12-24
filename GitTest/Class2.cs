ec1fd997b420d420f95844c3c3389865d1b8f259
RobertMarti <rm@eigerconsulting.ch>
2018-12-24 08:10:56 +0100

    class2

--- GitTest/Class2.cs
+++ GitTest/Class2.cs
@@ -1,1 +1,13 @@
-Class2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class Class2
+    {
+        //master
+    }
+}
